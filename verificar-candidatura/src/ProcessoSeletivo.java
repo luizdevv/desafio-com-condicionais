1a4620db67a3334c1f6995d97b28e017a55ec952
luizdevv <luizhttps05@gmail.com>
2024-05-13 13:55:50 -0300

    func: selecaoCandidato()

--- verificar-candidatura/src/ProcessoSeletivo.java
+++ verificar-candidatura/src/ProcessoSeletivo.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
@@ -9,8 +10,40 @@ public class ProcessoSeletivo {
         double salario = scanner.nextDouble();
     
         analisarCandidato(salario);
+        selecaoCandidato();
 
         scanner.close();
+    }
+
+    static void selecaoCandidato () {
+
+        String [] candidatos = {"FELIPE", "JOÃO", "LUIZ", "GUSTAVO", "RICARDO", "ALICE", "VITOR", "GUILHERME", "JOSÉ", "FERNANDO"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados <= 5 && candidatoAtual < candidatos.length) {
+            
+            String candidato = candidatos[candidatoAtual];
+            double salario = valorPretendido();
+
+            System.out.println("O candidato " + candidato + ". Solicitou este salário R$: " + salario);
+
+            if (salario <= salarioBase) {
+                
+                System.out.println( candidato + " foi selecionado.");
+
+                candidatosSelecionados++;
+            }
+
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido () {
+
+        return ThreadLocalRandom.current().nextDouble(1800, 2500);
     }
 
     static void analisarCandidato (double salarioPretendido) {
